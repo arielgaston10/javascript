1467a5e2f12afaa364f0db5b48c1e135acddeaf4
Ariel Choque Sirpa <arielgaston10@gmail.com>
2023-06-29 18:01:20 -0300

    3ra Entrega del proyecto final

--- productos.js
+++ productos.js
@@ -1,28 +1,47 @@
 const productos = [{
+    id: 1,
     nombre: "Royal Canin Medium Adult",
-    edad: "1 a 8 de edad",
-    precio: "11799",
-    cantidad: "15kg",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/combined/combined_1014",
+    precio: 11799,
+    cantidad: "15kg"
 },
 {
+    id: 2,
     nombre: "Pro Plan Adulto Razas Medianas",
-    edad: "1 a 9 de edad",
-    precio: "18559",
-    cantidad: "15kg",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/combined/combined_510",
+    precio: 18559,
+    cantidad: "15kg"
 },
 {
+    id: 3,
     nombre: "Purina Excellent Adult Maintenance Formula",
-    edad: "1 a 8 de edad",
-    precio: "24225",
-    cantidad: "20kg",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/desktop_purina-excellent-adult-mediumlarge-breed-chyr_159",
+    precio: 24225,
+    cantidad: "20kg"
 },
 {
+    id: 4,
     nombre: "Vital Can Balanced Adulto Raza Mediana",
-    edad: "1 a 9 de edad",
-    precio: "26000",
-    cantidad: "10kg",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/combined/combined_344",
+    precio: 26000,
+    cantidad: "10kg"
 },
 
+{
+    id: 5,
+    nombre: "Vital Can Balanced Adulto Raza Grande",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/combined/combined_349",
+    precio: 17999,
+    cantidad: "20kg"
+},
+{
+    id: 6,
+    nombre: "Pro Plan Puppy Razas Medianas",
+    foto: "https://s3-sa-east-1.amazonaws.com/mispichos.com.ar/mp_images/combined/combined_492",
+    precio: 13336,
+    cantidad: "6kg"
+}   
 
 
-]
+
+];
--- variables.js
+++ variables.js
@@ -1,36 +1,127 @@
-/*variable para ingresar el nombre*/
-let nombre=prompt('Ingresar tu nombre');
-
-while(nombre == '') {alert ('Nombre invalido');
-nombre = prompt('Ingresar tu nombre');
-}
-alert ('Bienvenido/a '+nombre);
-
 console.table(productos);
+const carro = [];
 
 function filtrarPorPrecio(precio){
     const filtrados = productos.filter((prod)=>prod.precio <= precio);
     return filtrados;
 }
-/* codigos de entrada */
-let preciomax = parseFloat(prompt('Ingresa el dinero max que puedas abonar 0-para salir'));
-while(preciomax != 0 ){
-    if(isNaN(preciomax) || preciomax < 0){
-        alert('Error. Ingrese un numero valido');
-    }else{
-        const prodsFiltrados = filtrarPorPrecio(preciomax);
-        console.table(prodsFiltrados);
+
+
+let contenedorProds = document.getElementById('misprods');
+let tablaBody = document.getElementById('tablabody');
+
+/*se utiliza dom*/
+function renderizarProductos(listaProds){
+    for(const prod of listaProds){
+        contenedorProds.innerHTML+=`
+        <div class="card col-md-2">
+  <img class="card-img-top" src="${prod.foto}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${prod.nombre}</h5>
+    <p class="card-text">$ ${prod.precio}</p>
+    <button id=${prod.id} class="btn btn-primary compra">Comprar</button>
+  </div>
+</div>
+        `;
     }
-    preciomax = parseFloat(prompt('Ingrese el dinero max que puedas abonar 0-para salir'));
+    /*evento */
+    let botones = document.getElementsByClassName('compra');
+    for(const boton of botones){
+     boton.addEventListener('click',()=>{
+        const prodACarro = productos.find((producto) => producto.id == boton.id);
+        console.log(prodACarro);
+
+        agregarACarrito(prodACarro);
+    })
+
+    /*evento del mouseover*/
+    boton.onmouseover = () => {
+      boton.classList.replace('btn-primary','btn-warning');
+    }
+
+    boton.onmouseout = () => {
+      boton.classList.replace('btn-warning','btn-primary');
+    }
+  }
+}
+
+renderizarProductos(productos);
+
+console.log(document.body);
+console.dir(document.body);
+
+let navegador = document.getElementById('navegador');
+console.log(navegador);
+console.log(navegador.innerHTML);
+navegador.style.body='#3EFF99';
+
+let titulo = document.getElementById('titulo');
+titulo.style.font='bold 35px monospace';
+
+let parrafos = document.getElementsByTagName('p');
+console.log(parrafos);
+parrafos[0].innerText = new Date().toLocaleDateString();
+
+
+/*agregando productos*/
+function agregarACarrito(producto){
+    carro.push(producto);
+    console.table(carro);
+    tablaBody.innerHTML += `
+      <tr>
+        <td>${producto.id}</td>
+        <td>${producto.nombre}</td>
+        <td>${producto.precio}</td>
+      </tr>
+    `;
+
+/*suma total de los productos agregados*/
+  let totalCarrito = carro.reduce((acumulador,producto)=>acumulador+producto.precio,0);
+
+  document.getElementById('total').innerText = 'Total a pagar $: '+totalCarrito;
+
 }
 
 
+for (let i = 0; i < localStorage.length; i++) {
+   let clave = localStorage.key(i);
+   console.log("Clave: "+ clave);
+   console.log("Valor: "+ localStorage.getItem(clave));
+}
+
+/almacenar los productos por json/
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+/*almacenamiento por producto*/
+for (const producto of productos) {
+    guardarLocal(producto.id, JSON.stringify(producto));
+}
+ 
+
+class Producto {
+  constructor(obj) {
+    this.nombre = obj.nombre.toUpperCase();
+    this.precio = parseFloat(obj.precio);
+  }
+  sumaIva() {
+    this.precio = this.precio * 1.21;
+  }
+}
+/*se obtiene los productos almacenados*/
+const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
+const productos2 = [];
 
 
+for (const objeto of almacenados)
+  productos2.push(new Producto(objeto));
 
+ for (const producto of productos2)
+  producto.sumaIva();
 
+ console.table(productos2); 
 
-
+ 
+  
 
 
 
